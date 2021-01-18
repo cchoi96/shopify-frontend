bbc4def5bebf22b0242fe4026f1742e73fa6292c
Chris Choi <chrischoi96@gmail.com>
2021-01-17 19:33:31 -0500

    Added more constants to .env file and fixed issue with OMDB api link

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,7 @@ require('dotenv').config();
 
 module.exports = Object.freeze({
   MAX_NOMINATIONS: 5,
-  SERVER_URL: process.env.REACT_APP_SERVER_URL
+  SERVER_URL: process.env.REACT_APP_SERVER_URL,
+  OMDB_API_KEY: process.env.REACT_APP_OMDB_API_KEY,
+  OMDB_API_URL: process.env.REACT_APP_OMDB_API_URL
 });
--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import axios from 'axios';
+import constants from '../constants';
 import './Search.css';
 
 const Search = ({ setResults }) => {
-  const API_KEY = '8a2ec1a4';
+  const OMDB_API_KEY = constants.OMDB_API_KEY;
+  const OMDB_API_URL = constants.OMDB_API_URL;
 
   const submit = query => {
-    axios.get(`http://www.omdbapi.com/?s=${query}&type=movie&apikey=${API_KEY}`)
+    axios.get(`${OMDB_API_URL}/?s=${query}&type=movie&apikey=${OMDB_API_KEY}`)
     .then(result => {
       const moviesData = result.data;
       const error = moviesData.Error;
       if (error) throw new Error(error);
       const searchResults = moviesData.Search;
       if (searchResults) setResults(moviesData.Search);
-    })
-    .catch(err => {
-
     });
   }
 
